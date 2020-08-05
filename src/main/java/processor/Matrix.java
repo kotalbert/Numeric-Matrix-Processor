c3bf6e8a537d43ed4db4e53250aa021feb960dba
pawel <pawel@daniluk.waw.pl>
2020-08-05 19:32:04 +0200

    Created Matrix class with empty matrix constructor.

---
+++ src/main/java/processor/Matrix.java
@@ -0,0 +1,33 @@
+package processor;
+
+
+
+public class Matrix {
+    public final int n;
+    public final int m;
+    private final int[][] matrix;
+
+    /**
+     * New empty matrix.
+     * @param n row number
+     * @param m column number
+     */
+    public Matrix(int n, int m) {
+        validateDimensions(n, m);
+        this.n = n;
+        this.m = m;
+        this.matrix = new int[n][m];
+    }
+
+
+    /**
+     * Check if legal matrix dimensions.
+     */
+    private static void validateDimensions(int n, int m) {
+        if ((n < 0 || m < 0)
+                || (n == 0 && m == 0))
+            throw new IllegalArgumentException(String.format("Illegal dimensions %d x %d", n, m));
+
+    }
+
+}
